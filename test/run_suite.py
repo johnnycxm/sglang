fde6948917ad08765a4fd0767ca53017b618aeb2
alisonshao <a.shao@wustl.edu>
2026-01-21 15:45:54 -0800

    Add stage-c-test-large-8-gpu-amd to AMD per-commit suites

--- test/run_suite.py
+++ test/run_suite.py
@@ -28,6 +28,7 @@ PER_COMMIT_SUITES = {
         "stage-b-test-large-2-gpu-performance-amd",
         "stage-b-test-small-1-gpu-accuracy-amd",
         "stage-b-test-large-2-gpu-accuracy-amd",
+        "stage-c-test-large-8-gpu-amd",
         "stage-c-test-large-8-gpu-amd-mi35x",
     ],
     HWBackend.CUDA: [
